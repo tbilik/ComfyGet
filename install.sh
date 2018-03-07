8075f220b7c3268b588511b900128992139cb173
Timothy Bilik <timothy@bilikfamily.com>
2018-03-07 15:06:44 -0500

    Gentoo & FreeBSD detection added

--- install.sh
+++ install.sh
@@ -25,6 +25,12 @@ elif [[ $(which eopkg 2> /dev/null) ]]; then
 elif [[ $(which crew 2> /dev/null) ]]; then
         echo -n "Chromebrew (an unoffical Chrome/Chromium OS package manager) has been detected. Is this correct? [y/n] "
         pm=chromebrew
+elif [[ $(which emerge 2> /dev/null) ]]; then
+        echo -n "A Gentoo or Gentoo-based system has been detected. Emerge will be used. Is this correct? [y/n] "
+        pm=emerge
+elif [[ $(which pkg 2> /dev/null) ]]; then
+        echo -n "You're one of those weird FreeBSD users, aren't you? If so, pkg will be used. Is this correct? [y/n] "
+        pm=pkg
 else
 	echo -n "A package manager has failed to be detected. If you proceed to install the program, termget will ask 
 you to set a package manager manually on first launch. Proceed to install? [y/n] "
