fdf800ffb38d5e90929f9a6557d3cfa3fac79a77
Timothy Bilik <timothy@bilikfamily.com>
2018-04-17 19:42:06 -0400

    comfyget links to termget.py; homebrew put higher in autodetection

--- install.sh
+++ install.sh
@@ -7,6 +7,9 @@ sleep 5
 if [[ $(which apt-get 2> /dev/null) ]]; then
 	echo -n "A Debian or Debian-based system has been detected. Apt-get will be used. Is this correct? [y/n] "
 	pm=apt-get
+elif [[ $(which homebrew 2> /dev/null) ]]; then
+        echo -n "Why do you torture yourself with using Mac OS? Fine, homebrew will be used. Is this correct? [y/n] "
+        pm=homebrew
 elif [[ $(which pacman 2> /dev/null) ]]; then
 	echo -n "An Arch or Arch-based system has been detected. Pacman will be used. Is this correct? [y/n] "
 	pm=pacman
@@ -31,9 +34,6 @@ elif [[ $(which emerge 2> /dev/null) ]]; then
 elif [[ $(which pkg 2> /dev/null) ]]; then
         echo -n "You're one of those weird FreeBSD users, aren't you? If so, pkg will be used. Is this correct? [y/n] "
         pm=pkg
-elif [[ $(which homebrew 2> /dev/null) ]]; then
-        echo -n "Why do you torture yourself with using Mac OS? Fine, homebrew will be used. Is this correct? [y/n] "
-        pm=homebrew
 else
 	echo -n "A package manager has failed to be detected. If you proceed to install the program, termget will ask 
 you to set a package manager manually on first launch. Proceed to install? [y/n] "
@@ -55,17 +55,18 @@ you to set a package manager manually on first launch. Proceed to install? [y/n]
 	fi
 fi
  
-echo -e "\e[1m>>> setting up directories"
+echo ">>> setting up directories"
 sudo mkdir /etc/comfyget
-echo ">>> copying program to ~/.termget"
-sudo cp termget.py /usr/local/bin/comfyget
+echo ">>> copying program to /usr/local/bin"
+sudo cp termget.py /usr/local/bin/
+sudo cp comfyget /usr/local/bin/
 echo ">>> generating package file"
 sudo bash -c "echo -n $pm > /etc/comfyget/termget-package-manager"
-echo -e ">>> creating alias into .bashrc\e[0m"
+# echo -e ">>> creating alias into .bashrc\e[0m"
 # echo 'alias comfyget="python3 ~/.termget/termget.py"' >> ~/.bashrc
 # alias comfyget="python3 ~/.termget/termget.py"
 echo ""
 echo "Note: From now on, any changes made to the code"
-echo "must be ran under ./update.sh"
+echo "can be updated by re-running the install script"
 echo ""
-echo -e "\e[32m\e[1m>>> Done!\e[0m"
+echo ">>> Done!"
